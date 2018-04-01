b8eb36c283b48cb5e57ef33fd7640de0474a93b6
Jeff Vongkoth <jvongkoth@gmail.com>
2018-04-01 16:49:14 -0500

    added arrays and trying to form logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+var nissan = ["n", "i", "s","s", "a", "n"]
+var porsche = ["p","o","r","s","c","h","e" ]
+var ferrari = ["f","e","r","r","a","r","i"]
+var toyota = ["t","o","y","o","t","a"]
+var mclaren = ["m","c","l","a","r","e","n"]
+var koenigsegg = ["k","o","e","n","i","g","s","e","g","g"]
+var numberOfGuessesRemaining = 15;
+
+document.onkeyup = function(event) {
+    var prevGuessed = key already entered wont let enter again;
+
+var userGuess = event.key.toLowercase();
+
+var computerGuess = nissan[Math.floor(Math.random() * nissan.length)];
+
+if ((userGuess === "n") || (userGuess === "i") || (userGuess === "s")|| (userGuess === "a")) {
+    
+
+}
