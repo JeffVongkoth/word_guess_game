3df6329a2b40b5612e635d96c1843ea591050a05
Jeff Vongkoth <jvongkoth@gmail.com>
2018-04-01 23:05:40 -0500

    updated array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,15 @@
-var nissan = ["n", "i", "s","s", "a", "n"]
-var porsche = ["p","o","r","s","c","h","e" ]
-var ferrari = ["f","e","r","r","a","r","i"]
-var toyota = ["t","o","y","o","t","a"]
-var mclaren = ["m","c","l","a","r","e","n"]
-var koenigsegg = ["k","o","e","n","i","g","s","e","g","g"]
+var answers = ["porsche","ferrari","toyota","mclaren","koenigsegg","nissan"]
+
+var chr = String.fromCharCode(97 + 0);
 var numberOfGuessesRemaining = 15;
 
 document.onkeyup = function(event) {
-    var prevGuessed = key already entered wont let enter again;
+   // var prevGuessed = key already entered wont let enter again;
 
 var userGuess = event.key.toLowercase();
 
-var computerGuess = nissan[Math.floor(Math.random() * nissan.length)];
+var computerGuess = blank[Math.floor(Math.random() * blank.length)]; }
 
-if ((userGuess === "n") || (userGuess === "i") || (userGuess === "s")|| (userGuess === "a")) {
-    
-
+GuessRight: function() {
+    console.log(answer[0])
 }
