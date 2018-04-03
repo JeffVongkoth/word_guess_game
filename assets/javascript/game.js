90d0a9c353ccc3a9213c40888b7c0b1591341b99
Jeff Vongkoth <jvongkoth@gmail.com>
2018-04-03 16:57:47 -0500

    added more js and html text

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@ var answers = ["porsche","ferrari","toyota","mclaren","koenigsegg","nissan"]
 var alreadyAnswered = []    // var alreadyAnswered= key already entered wont let enter again;
 var correctAnswer = [] // fill in with var push or something
 var numberOfGuessesRemaining = 15;
+var images = ["f1.jpg","f40.jpg","gtr.jpg","supra.jpg","porsche.jpg","koenigsegg.jpg"]
 function myFunction() {
     correctAnswer.push(userGuess)
 }
@@ -14,7 +15,14 @@ document.onkeyup = function(event) {
 var userGuess = event.key.toLowercase();
 
 var randomWord = answers[Math.floor(Math.random() * answers.length)]; }
-
+if (event.key === answers.porsche[0]
 GuessRight: function() {
     console.log(answer[0])
 }
+
+function displayImage(){
+
+    //the first statement should generate a random number in the range 0 to 6 (the subscript values of the image file names in the imagesArray)
+    var num = Math.floor(Math.random() * 6); // 0...6
+    //the second statement display the random image from the imagesArray array in the canvas image using the random number as the subscript value
+    document.images.src = imagesArray[num];
