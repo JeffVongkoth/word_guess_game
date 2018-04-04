deebf8cda12c9cc6e48e49e325bf8333b99a6953
Jeff Vongkoth <jvongkoth@gmail.com>
2018-04-04 17:09:24 -0500

    fixed some js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,39 @@
-var answers = ["porsche","ferrari","toyota","mclaren","koenigsegg","nissan"]
-var alreadyAnswered = []    // var alreadyAnswered= key already entered wont let enter again;
-var correctAnswer = [] // fill in with var push or something
+//get a random word
+
+//create underscores based on random words
+
+//get user guess
+
+//check if user guess is right 
+
+//if right add letter from answer and replace underscore
+
+//if wrong add letter to wrong guess
+
+
+
+var answer = ["porsche","ferrari","toyota","mclaren","koenigsegg","nissan"];
+
+var wrongGuess = [];   
+
 var numberOfGuessesRemaining = 15;
-var images = ["f1.jpg","f40.jpg","gtr.jpg","supra.jpg","porsche.jpg","koenigsegg.jpg"]
-function myFunction() {
-    correctAnswer.push(userGuess)
+
+var images = ["f1.jpg","f40.jpg","gtr.jpg","supra.jpg","porsche.jpg","koenigsegg.jpg"];
+
+var randomAnswer = answer[Math.floor(Math.random() * answer.length)];
+
+console.log(randomAnswer);
+
+var chosenAnswer = answer[randomAnswer];
+
+document.onkeyup = function() { 
+    var userGuess = event.key.toLowercase();  
+
+    var generateUnderscore = function(){
+        for (var i = 0; i < chosenAnswer.length; i++) {
+        underScore.push('_');
+        }
+        return underScore;
+        }
+
 }
-
-
-
-document.onkeyup = function(event) {
-
-
-var userGuess = event.key.toLowercase();
-
-var randomWord = answers[Math.floor(Math.random() * answers.length)]; }
-if (event.key === answers.porsche[0]
-GuessRight: function() {
-    console.log(answer[0])
-}
-
-function displayImage(){
-
-    //the first statement should generate a random number in the range 0 to 6 (the subscript values of the image file names in the imagesArray)
-    var num = Math.floor(Math.random() * 6); // 0...6
-    //the second statement display the random image from the imagesArray array in the canvas image using the random number as the subscript value
-    document.images.src = imagesArray[num];
